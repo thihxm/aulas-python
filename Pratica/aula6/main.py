da157a31fdab52919ce05dded695358f9164ad00
Thiago Medeiros <thiago@mentores.com.br>
2020-06-05 19:47:04 -0300

    Agenda finalizada com Banco

--- Pratica/aula6/main.py
+++ Pratica/aula6/main.py
@@ -4,6 +4,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, NumericProperty
 from random import randint
+from AgendaIntegrada import BancodeDadosContatos, Contatos
 
 class Linha(BoxLayout):
     nome = StringProperty('')
@@ -20,17 +21,74 @@ class Principal(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
     
-        self.ids.listagem.data = [{'id': x, 'nome': str(x + 1), 'telefone': f'9{str(randint(10000000, 99999999))}'} for x in range(20)]
+    def popular_listagem(self, busca):
+        self.ids.listagem.data = list()
+
+        db = BancodeDadosContatos()
+        contatos = db.ListarContatosPorNome(busca)
+
+        for contato in contatos:
+            self.ids.listagem.data.append({ 'id': contato.id, 'nome': contato.nome })
+    
+    def novo_contato(self):
+        App.get_running_app().id_atual = 0
+
+        self.manager.transition.direction = 'left'
+        self.manager.current = 'detalhes'
     
 class Visualizar(Screen):
     def preencher_detalhes(self):
         id = App.get_running_app().id_atual
-        
-        self.ids.nome_contato.text = str(id)
+
+        if id == 0:
+            self.ids.txNome.text = ''
+            self.ids.txTelefone.text = ''
+            self.ids.txEmail.text = ''
+            self.contato_atual = None
+            self.bloquear_campos(False)
+        else:
+            db = BancodeDadosContatos()
+            self.contato_atual = db.BuscaContatoPorId(id)
+            self.ids.txNome.text = self.contato_atual.nome
+            self.ids.txTelefone.text = self.contato_atual.telefone
+            self.ids.txEmail.text = self.contato_atual.email
+            self.bloquear_campos(True)
+
+    def bloquear_campos(self, bloquear):
+        self.ids.txNome.disabled = bloquear
+        self.ids.txTelefone.disabled = bloquear
+        self.ids.txEmail.disabled = bloquear
+        if bloquear:
+            self.ids.btnEditar.text = 'Editar'
+            self.ids.btnExcluir.disabled = False
+        else:
+            self.ids.btnEditar.text = 'Salvar'
+            self.ids.btnExcluir.disabled = True
 
     def voltar(self):
         self.manager.transition.direction = 'right'
         self.manager.current = 'principal'
+
+    def editar(self):
+        if self.ids.btnEditar.text == 'Editar':
+            self.bloquear_campos(False)
+        else:
+            db = BancodeDadosContatos()
+            if not self.contato_atual:
+                novo_contato = Contatos(None, self.ids.txNome.text, self.ids.txTelefone.text, self.ids.txEmail.text)
+                db.CadastrarContato(novo_contato)
+                self.voltar()
+            else:
+                self.contato_atual.nome = self.ids.txNome.text
+                self.contato_atual.telefone = self.ids.txTelefone.text
+                self.contato_atual.email = self.ids.txEmail.text
+                db.AtualizarContato(self.contato_atual)
+                self.bloquear_campos(True)
+
+    def excluir(self):
+        db = BancodeDadosContatos()
+        db.ExcluirContato(self.contato_atual)
+        self.voltar()
 
         
 class AgendaContatosApp(App):
